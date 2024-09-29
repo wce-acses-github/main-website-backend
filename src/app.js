6dc95f03f28f5e7229a5d885cc8ab83bc1322096
om7057 <kulkarniom7057@gmail.com>
2024-09-29 23:17:53 +0530

    Increased Data limit

--- src/app.js
+++ src/app.js
@@ -23,8 +23,8 @@ const corsOptions = {
 app.use(cors(corsOptions));
 
 // Middlewares
-app.use(express.json({ limit: "16kb" }));
-app.use(express.urlencoded({ extended: true, limit: "16kb" }));
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ extended: true, limit: "50mb" }));
 app.use(express.static("public"));
 
 // Routes
